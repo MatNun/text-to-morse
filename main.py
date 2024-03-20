b932a1604b1ebdb12daf16f087e2652f4c073255
Mateus Nunes <mat.nun.san@gmail.com>
2024-03-20 20:14:43 -0300

    Finish initial code.

--- main.py
+++ main.py
@@ -1,3 +1,33 @@
-import requests
-import beautifulsoup4
+import sys
 
+morse_code = {
+    'A': '.-', 'B': '-...', 'C': '-.-.', 'D': '-..', 'E': '.', 'F': '..-.', 'G': '--.', 'H': '....',
+    'I': '..', 'J': '.---', 'K': '-.-', 'L': '.-..', 'M': '--', 'N': '-.', 'O': '---', 'P': '.--.',
+    'Q': '--.-', 'R': '.-.', 'S': '...', 'T': '-', 'U': '..-', 'V': '...-', 'W': '.--', 'X': '-..-',
+    'Y': '-.--', 'Z': '--..',
+    '0': '-----', '1': '.----', '2': '..---', '3': '...--', '4': '....-', '5': '.....', '6': '-....',
+    '7': '--...', '8': '---..', '9': '----.',
+    '.': '.-.-.-', ',': '--..--', '?': '..--..', '\'': '.----.', '!': '-.-.--', '/': '-..-.', '(': '-.--.',
+    ')': '-.--.-', '&': '.-...', ':': '---...', ';': '-.-.-.', '=': '-...-', '+': '.-.-.', '-': '-....-',
+    '_': '..--.-', '"': '.-..-.', '$': '...-..-', '@': '.--.-.', ' ': '/'
+}
+
+print("Welcome to the Morse Code Translator! ")
+user_data = input('Please write what you want to translate in morse code: ')
+translated_data = []
+
+counter = 1
+
+while counter <= len(user_data):
+    try:
+        letter = morse_code[user_data[counter-1].upper()]
+        translated_data.append(letter)
+
+    except KeyError:
+        print(f"Sorry, I can't translate '{user_data[counter-1]}'. Try again.")
+        sys.exit()
+
+    counter += 1
+
+print("Your message translated in Morse Code is: ")
+print(' '.join(translated_data))
